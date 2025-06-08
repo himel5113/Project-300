e45cbddf9b3d997d406b68047f504744f423b32d
sudipto-dey-himel <sudiptodeyhimel02@gmail.com>
2025-06-08 13:06:13 +0600

    added features

--- lost_and_found_portal_MU/app/urls.py
+++ lost_and_found_portal_MU/app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lost/', views.lostItems, name = "lostItems"),
+    path('found/', views.foundItems, name = "foundItems"),
 ]
--- lost_and_found_portal_MU/app/views.py
+++ lost_and_found_portal_MU/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'app/basic/home.html')
+
+
+
+def lostItems(request):
+    return render(request, 'app/basic/lost.html')
+
+
+def foundItems(request):
+    return render(request, 'app/basic/found.html')
