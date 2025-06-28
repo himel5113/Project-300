7b063d9e4a212c484a2f080179243255f8c80158
sudipto-dey-himel <sudiptodeyhimel02@gmail.com>
2025-06-28 21:15:57 +0600

    set the claim request button functionality

--- lost_and_found_portal_MU/lost_and_found_portal_MU/settings.py
+++ lost_and_found_portal_MU/lost_and_found_portal_MU/settings.py
@@ -130,3 +130,11 @@ LOGIN_URL = '/signin/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sudiptodeyhimel02@gmail.com'
+EMAIL_HOST_PASSWORD = 'fsngfnlumykdxjpf'
