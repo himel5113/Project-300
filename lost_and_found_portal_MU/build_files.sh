e98b40ce740affc739db79b5d07d899d69a63182
Md. Tanimur Rahman <t5mishkat@gmai.com>
2025-07-28 20:17:25 +0600

    fix config

--- lost_and_found_portal_MU/build_files.sh
+++ lost_and_found_portal_MU/build_files.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+set -e
+
+# Create and activate virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+python3.9 manage.py collectstatic --noinput --clear
